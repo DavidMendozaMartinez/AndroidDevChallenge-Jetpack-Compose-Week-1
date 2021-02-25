636f960c53b12f979fa1bfeab1e2fac1ce7a593f
David Mendoza <david.mendoza.mnez@gmail.com>
2021-02-25 06:52:32 +0100

    Create an adoption application when adopt button is clicked

--- app/src/main/java/com/example/androiddevchallenge/ui/detail/DetailActivity.kt
+++ app/src/main/java/com/example/androiddevchallenge/ui/detail/DetailActivity.kt
@@ -1,10 +1,12 @@
 package com.example.androiddevchallenge.ui.detail
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import com.example.androiddevchallenge.R
 import com.example.androiddevchallenge.model.Puppy
 import com.example.androiddevchallenge.ui.theme.MyTheme
 
@@ -19,10 +21,24 @@ class DetailActivity : AppCompatActivity() {
             MyTheme {
                 Surface(color = MaterialTheme.colors.background) {
                     intent.getParcelableExtra<Puppy>(EXTRA_PUPPY)?.let {
-                        DetailScreen(it) {}
+                        DetailScreen(it) { puppy ->
+                            createAdoptionApplication(puppy.name)
+                        }
                     }
                 }
             }
+        }
+    }
+
+    private fun createAdoptionApplication(name: String) {
+        val intent = Intent(Intent.ACTION_SEND)
+            .setType("text/plain")
+            .putExtra(Intent.EXTRA_EMAIL, arrayOf(getString(R.string.adoption_instructions_email)))
+            .putExtra(Intent.EXTRA_SUBJECT, getString(R.string.new_adoption_application, name))
+            .putExtra(Intent.EXTRA_TEXT, getString(R.string.adoption_application, name))
+
+        if (packageManager?.resolveActivity(intent, 0) != null) {
+            startActivity(intent)
         }
     }
 }
